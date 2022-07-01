38f92fe10e3c7508e16753d9fadf74391ccfe422
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-07-01 11:28:18 +0300

    Initial commit

---
+++ src/com.luv2code.aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,28 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+    @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+        System.out.println("\n=====>>> Executing @Before advice on addAccont()");
+
+
+        // display the method signature
+        MethodSignature methoSig = (MethodSignature) theJoinPoint.getSignature();
+        System.out.println("Method :" + methoSig);
+
+        // display the method arguments
+    }
+
+}
